744822df50def37c2278523e2e0453c6a5caa43a
Halil Yıldırım <71269065+ydmhalil@users.noreply.github.com>
2024-11-13 15:00:52 +0300

    Create LinkedList.cpp

---
+++ LinkedList.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* next;
+
+    Node(int data) {
+        this->data = data;
+        this->next = nullptr;
+    }
+};
+
+class LinkedList {
+private:
+    Node* head;
+
+public:
+    LinkedList() {
+        Node* head = nullptr;
+    }
+
+    void insertBegin(int data) {
+        Node* newNode = new Node(data);
+        newNode->next = head;
+        head = newNode;
+    }
+
+    void insertLast(int data) {
+        Node* newNode = new Node(data);
+
+        if (head == nullptr) {
+            head = newNode;
+        } else {
+            Node* temp = head;
+            while (temp->next != nullptr) {
+                temp = temp->next;
+            }
+            temp->next = newNode;
+        }
+    }
+
+    void print() {
+        Node* temp = head;
+        while (temp != nullptr) {
+            cout << temp->data << " -> ";
+            temp = temp->next;
+        }
+        cout << "nullptr" << endl;
+    }
+
+};
+
+int main() {
+    LinkedList list;
+
+    list.insertLast(10);
+    list.insertLast(20);
+    list.insertLast(30);
+
+    cout << "Linked List: ";
+    list.print();
+
+    return 0;
+}
